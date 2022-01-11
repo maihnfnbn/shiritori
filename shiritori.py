8659ec8d3174c002a752bcd6c6b585d4d03381cc
Mai Luu <mailuungoc96@gmail.com>
2022-01-11 15:09:17 +0900

    finished welcome

--- shiritori.py
+++ shiritori.py
@@ -6,19 +6,45 @@ class player():
 open_dict = open("C:\python\shiritori\word.txt", "r").read()
 dict = open_dict.split('\n')
 
+from random import randint
+last_letter = ''
+difficult_letters = ['q','j','z','x','v','k','w','y','f']
+play_word = ''
+current_player = {}
+
 def welcome():
+    global play_word
+    play_word = dict[randint(0,len(dict) - 1)]
+
     name_1 = input('First player please enter your name: ')
     name_2 = input('Second player please enter your name: ')
     player_1 = player(name_1)
     player_2 = player(name_2)
+    players = {1: player_1, 2: player_2}
 
-    print('Hi {} and {}! Welcome to Shiritori!'.format(player_1.name, player_2.name))
+    print('Hi {} and {}! Welcome to Shiritori!'.format(name_1, name_2))
     print('Players are required to say a word which begins with the final kana of the previous word.')
-    print('Enter "OK" when you are ready to play! Computer will choose the first player and first letter')
+    print('Computer will choose the first player and first letter')
+    rand_current_player = randint(1,2)
+    current_player = players[rand_current_player]
+    play_word = input('{} go first, your word is {}'.format(current_player.name, play_word))
 
-import os
+welcome()
+
+
 def check_word(last_letter, word, player):
-    if word[0] == last_letter:
+    global dict
+    if word[0] == last_letter: #if first letter of input is last letter
         if word in dict:
-            print('correct')
-    else: print('not correct')
+            if word[0] in difficult_letters:
+                player.score += 2
+            else: player.score += 1
+            player.score += len(word)
+            dict.pop(word)
+    else: word = input('word incorrect. Try again! ')
+
+def play():
+    next_player= {}
+    welcome()
+    last_letter = play_word[-1]
+    check_word(last_letter, play_word, next_player)
